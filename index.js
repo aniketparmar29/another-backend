1adc3f422e283950e23cb56ca58888ed7150f4a4
aniketparmar29 <aniketparmar068@gmail.com>
2023-04-08 22:39:29 +0530

    SDF

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+  
+
 const pool = mysql.createPool({
     connectionLimit: 100,
     host: process.env.HOST,
